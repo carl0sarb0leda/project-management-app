a3ff8e234056af1f5e10d464d65527889863c2a4
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-15 21:42:36 +1200

    Add graphql instance to express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { createHandler } = require("graphql-http/lib/use/express");
+const schema = require("./schema/schema");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+app.all("/graphql", createHandler({ schema, graphiql: true }));
 
 app.listen(port, console.log(`The server is running in port ${port}`));
