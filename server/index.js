d3a0c299b519507c2d1fffc602e2142a0d719989
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-15 22:36:19 +1200

    Update server with mongo config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const { createHandler } = require("graphql-http/lib/use/express");
-const schema = require("./schema/schema");
-require("dotenv").config();
-const port = process.env.PORT || 5000;
-const app = express();
-app.all("/graphql", createHandler({ schema, graphiql: true }));
+const connectDB = require("./config/db");
 
+const schema = require("./schema/schema");
+const port = process.env.PORT || 5000;
+
+const app = express();
+//Db instance
+connectDB();
+//Graphql instance
+app.use("/graphql", createHandler({ schema, graphiql: true }));
+//Port
 app.listen(port, console.log(`The server is running in port ${port}`));
