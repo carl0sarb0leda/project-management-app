df4c654a6fe71bf53bdc9350e74d81595567dee2
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-16 19:36:59 +1200

    Add cors in server config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { createHandler } = require("graphql-http/lib/use/express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 //Db instance
 connectDB();
+//CORS
+app.use(cors());
 //Graphql instance
 app.use("/graphql", createHandler({ schema, graphiql: true }));
 //Port
