b800f98596430a517e275fa5f2c9d65b279568b0
carl0sarb0leda <carlosaepn@gmail.com>
2023-04-29 19:28:18 +1200

    Update cache logic merge

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,7 +4,33 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          clients: {
+            merge(existing, incoming) {
+              /** While returning only the "incoming" data may be
+               * appropriate for some use cases, it's important to understand
+               * that this approach will cause the cached data to be completely overwritten
+               * every time new data is fetched from the server.
+               * This can lead to performance issues if you are frequently
+               * re-fetching large amounts of data. */
+              // if (existing && incoming) {
+              //   return [...existing, ...incoming]; // merging rather than overwrite
+              // }
+              return incoming;
+            },
+          },
+          projects: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
